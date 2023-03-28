90c01c3b083091f832424de08e9ac06b484441f0
Marília Lorenzato <mariliaglorenzato@gmail.com>
2023-03-27 22:08:00 -0300

    Initial commit

---
+++ modules/books/functions/worker/handler.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const dynamo = require('../../../../shared/lib/dynamo');
+const sqs = require('../../../../shared/lib/sqs');
+
+const DYNAMO_TABLE_BOOKS = process.env.DYNAMO_TABLE_BOOKS || 'books';
+const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL || 'book';
+
+let lastIntervalID;
+
+/**
+ * This is a Worker example 
+ * It's a simple POC to update DynamoDB itens based on hashkey value
+ * stored on SQS Queue
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+module.exports.worker = (event, context, callback) => {
+
+    if (lastIntervalID) {
+        clearInterval(lastIntervalID);
+    }
+    
+    //Get records on SQS to update a DynamoDB Table
+    lastIntervalID = setInterval(() => {
+
+        sqs.consumeQueue(1, SQS_QUEUE_URL)
+            .then(poll => {
+
+                if (!poll.Messages) {
+                    return;
+                } else {
+                    poll.Messages.forEach(message => {
+
+                        const item = JSON.parse(message.Body);
+
+                        //Update item on DynamoDB and remove from Queue
+                        _updateRecord(item.hashkey)
+                            .then(success => sqs.removeFromQueue(message))
+                            .catch(err => console.log(err));
+
+                    });
+                }
+
+        }).catch(err => console.log(err));
+
+    }, 100);
+
+};
+
+/**
+ * Update flag on DynamoDB Item
+ * @param {*} hashkey 
+ */
+const _updateRecord = hashkey => {
+
+    const key = { hashkey: hashkey };
+    const expression = "set updated_by_worker = :flag";
+
+    const values = {
+        ":flag": 1
+    };
+
+    return dynamo.update(key, expression, values, DYNAMO_TABLE_BOOKS);
+}
